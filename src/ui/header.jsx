79b3f2b38ddffdb37cc14b1b369364ed3b8de826
Kareem Taiye <olayinkahassan117@gmail.com>
2025-06-06 21:36:04 +0100

    starting the pizza website

---
+++ src/ui/header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function header() {
+  return (
+    <header>
+      <Link to="/"> Pizza Exc.</Link>
+
+      <p>Kareem</p>
+    </header>
+  );
+}
+
+export default header;
